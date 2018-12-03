2200a18b313036d2e49529d894b79cec7b264941
empyrean987 <knoxm1@gmail.com>
2018-12-02 21:35:45 -0600

    updating databags location

--- chef-solo/controller_solo.rb
+++ chef-solo/controller_solo.rb
@@ -1,3 +1,4 @@
 file_cache_path "/home/ubuntu/chef_kubernetes/chef-solo/"
 cookbook_path "/home/ubuntu/chef_kubernetes/cookbooks"
 json_attribs "/home/ubuntu/chef_kubernetes/chef-solo/node.json"
+data_bag_path "/home/ubuntu/chef_kubernetes/chef-solo/data_bags/"
