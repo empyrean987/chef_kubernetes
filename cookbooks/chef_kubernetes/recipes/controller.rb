d36a6a16e427529ff5ebc4946c42b1771f8e7e00
empyrean987 <knoxm1@gmail.com>
2018-12-02 23:01:50 -0600

    updates

--- cookbooks/chef_kubernetes/recipes/controller.rb
+++ cookbooks/chef_kubernetes/recipes/controller.rb
@@ -3,5 +3,6 @@
 # Recipe:: controller.rb
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+include_recipe 'kubernetes'
 include_recipe 'kubernetes::etcd'
 include_recipe 'kubernetes::master'
